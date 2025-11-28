3112a29909af0cf3e25e2d85b3656f76fc57645a
DenisPinzin <d.pinzinsro@gmail.com>
2025-11-28 19:06:18 +0100

    [NEW] finish contact & add projets + carousel

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -2,6 +2,8 @@ const menu = document.querySelector('.menu')
 const nav = document.querySelector('nav')
 let count = 0
 
+//MENU BURGER
+
 menu.addEventListener('click', function (menu){
     if (count == 0) {
         nav.classList.add('nav-menu')
@@ -21,3 +23,37 @@ menu.addEventListener('click', function (menu){
 //         return
 //     }
 // })
+
+//CAROUSEL
+// Récupère tous les éléments
+const images = document.querySelectorAll('.carousel img');
+const leftArrow = document.getElementById('left-arrow');
+const rightArrow = document.getElementById('right-arrow');
+
+let currentIndex = 0;
+
+// Fonction pour afficher l'image active
+function showImage(index) {
+  images.forEach((img, i) => {
+    img.classList.toggle('active', i === index);
+  });
+}
+
+
+// Affiche la première image au départ
+showImage(currentIndex);
+
+// Flèche droite → image suivante
+rightArrow.addEventListener('click', () => {
+  currentIndex = (currentIndex + 1) % images.length; // boucle
+  showImage(currentIndex);
+});
+
+// Flèche gauche → image précédente
+leftArrow.addEventListener('click', () => {
+  currentIndex = (currentIndex - 1 + images.length) % images.length; // boucle inverse
+  showImage(currentIndex);
+});
+
+
+
